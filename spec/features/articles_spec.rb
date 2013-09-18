68c904ee1a52b71d6595f56a3d27411ad9c6be7d
Matthew Wean <matthew.wean@gmail.com>
2013-09-18 00:24:05 -0700

    Disable pagination for now
    Using distinct: true with Ransack interfering with Kaminari

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -8,17 +8,17 @@ feature 'visiting the articles index' do
     expect(page).to have_css('table.articles tr', text: article.title)
   end
 
-  scenario 'articles are paginated' do
-    Article.paginates_per(1)
-    article1 = FactoryGirl.create(:article, pages: '1')
-    article2 = FactoryGirl.create(:article, pages: '2')
-    visit articles_path
+  # scenario 'articles are paginated' do
+  #   Article.paginates_per(1)
+  #   article1 = FactoryGirl.create(:article, pages: '1')
+  #   article2 = FactoryGirl.create(:article, pages: '2')
+  #   visit articles_path
 
-    expect(page).to have_content(article1.title)
-    expect(page).to have_no_content(article2.title)
+  #   expect(page).to have_content(article1.title)
+  #   expect(page).to have_no_content(article2.title)
 
-    click_on 'Next'
+  #   click_on 'Next'
 
-    expect(page).to have_content(article2.title)
-  end
+  #   expect(page).to have_content(article2.title)
+  # end
 end
