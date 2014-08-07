7e295da00d043d2badd30dbf0a3dfff803f10f4a
Diana Salsbury <diana.salsbury@gmail.com>
2014-08-06 21:47:28 -0700

    fix(pdfs):allow large file uploads through small carrierwave update

--- app/uploaders/pdf_uploader.rb
+++ app/uploaders/pdf_uploader.rb
@@ -3,6 +3,14 @@ require 'carrierwave'
 
 class PdfUploader < CarrierWave::Uploader::Base
   storage :fog
+  
+  def move_to_cache
+    true
+  end
+  
+  def move_to_store
+    true
+  end
 
   def store_dir
   end
